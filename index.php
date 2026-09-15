2acb32737e0db420594d1a92e546f6f056433c5d
Lucas <lucasemanuelbrazleite@gmail.com>
2026-09-15 08:11:21 -0300

    add o php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela-inicial</title>
+</head>
+<body>
+    <div class="">
+        <div class="retangulo">
+            <div class="parte2">
+            </div>
+            <div class="parte2">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
